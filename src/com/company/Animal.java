cb9651b4797391d3e6062b867e83caf2b044ad8d
rantochowski <rafal.antochowski@etrust.pl>
2021-05-31 23:33:53 +0200

    Adding more functionality to code

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,50 @@
 package com.company;
 
 public class Animal {
+
+    final String species;
+    private Double weight;
+
+    public Animal(String species) {
+        this.species = species;
+        weight = getWeight();
+
+        if (species.equals("dog")) {
+            setWeight(10.0);
+        }
+
+        if (species.equals("cat")) {
+            setWeight(2.5);
+        }
+
+        if (species.equals("elephant")) {
+            setWeight(104.00);
+        }
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public void feed() {
+        if (weight.equals(0.0)) {
+            System.out.println("Twój zwierzak nie żyje! Jak możesz nakarmić trupa?");
+        } else {
+            weight = getWeight();
+            weight += 1;
+        }
+    }
+
+    public void takeForAWalk() {
+        if (weight.equals(0.0)) {
+            System.out.println("Twój zwierzak nie żyje! Jak możesz wyjść na spacer z trupem?");
+        } else {
+            weight = getWeight();
+            weight -= 1;
+        }
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Human John = new Human();
+	    John.pet = new Animal("elephant");
+
+        John.pet.feed();
+	    John.pet.takeForAWalk();
+
+        for (int i = 0; i <= 10; i++) {
+            John.pet.feed();
+        }
+
+        for (int i = 0; i <= 10; i++) {
+            John.pet.takeForAWalk();
+        }
+
+        System.out.println(John.pet.getWeight());
+
     }
 }
