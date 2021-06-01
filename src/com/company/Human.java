769de7d845d34ce45148970d7e10e1432002457d
rantochowski <rafal.antochowski@etrust.pl>
2021-06-01 20:02:32 +0200

    Exercises 3 and 4

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,6 +1,39 @@
 package com.company;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ArrayList;
 
 public class Human {
     Animal pet;
     Car car;
+    private Double salary;
+    SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+    Date salaryDate = new Date();
+    String lastSalaryDate = dateFormatter.format(salaryDate);
+    ArrayList<String> salaryGettersList = new ArrayList<String>();
+
+    public void setSalary(Double salary){
+        if(salary < 0.0) {
+            System.out.println("Nie można przypisać ujemnej wartości do pensji.");
+        } else {
+            this.salary = salary;
+            System.out.println("Nowe dane zostały wysłane do systemu księgowego \nProszę pamiętać o odebraniu aneksu do umowy od pani Hani z kadr \nZUS i US już wiedzą o zmianie wypłaty i nie ma sensu ukrywać dochodu");
+        }
+    }
+
+    public Double getSalary(){
+        salaryGettersList.add(lastSalaryDate);
+        String salaryString = String.valueOf(salary);
+        salaryGettersList.add(salaryString);
+
+        if(salaryGettersList.size() > 3) {
+            System.out.println("Data ostatniego przeglądu stanu konta: " + salaryGettersList.get(salaryGettersList.size() - 4));
+            System.out.println("Ostatni stan konta: " + salaryGettersList.get(salaryGettersList.size() - 3));
+        } else {
+            System.out.println("Data ostatniego przeglądu stanu konta: " + lastSalaryDate);
+            System.out.println("Ostatni stan konta: " + salary);
+        }
+
+        return salary;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,18 +7,27 @@ public class Main {
 	    John.pet = new Animal("elephant");
 	    John.car = new Car("Cayenne", "Porsche", "Red", 2009);
 
-        John.pet.feed();
-	    John.pet.takeForAWalk();
+//        John.pet.feed();
+//	    John.pet.takeForAWalk();
+//
+//	    John.getSalary();
+//
+//        for (int i = 0; i <= 10; i++) {
+//            John.pet.feed();
+//        }
+//
+//        for (int i = 0; i <= 10; i++) {
+//            John.pet.takeForAWalk();
+//        }
 
-        for (int i = 0; i <= 10; i++) {
-            John.pet.feed();
-        }
+        John.setSalary(4999.0);
+        John.getSalary();
+        John.setSalary(5000.0);
+        John.getSalary();
+        John.setSalary(3500.0);
+        John.getSalary();
 
-        for (int i = 0; i <= 10; i++) {
-            John.pet.takeForAWalk();
-        }
-
-        System.out.println(John.car.model);
+        System.out.println(John.salaryGettersList);
 
     }
 }
